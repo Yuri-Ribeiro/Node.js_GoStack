c30f7b404866e0d0361683a6f7f3df9f4a0a53d1
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-11-01 16:54:08 -0300

    POST; PUT e DELETE

--- module01/index.js
+++ module01/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
-const users = ['Yuri', 'Even', 'Solange', 'Junior']
+const users = ['Yuri', 'Even', 'Maria', 'Solange', 'Junior']
+
+server.use(express.json());
 
 // query params
 server.get('/test', (req, res) => {
@@ -16,6 +18,35 @@ server.get('/users/:index', (req, res) => {
     const { index } = req.params;
 
     return res.json(users[index]);
+})
+
+server.get('/users', (_, res) => {
+    return res.json(users);
+})
+
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+})
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body; 
+
+    users[index] = name
+
+    return res.json(users);
+})
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+
+    return res.sendStatus(204);
 })
 
 server.listen(port, () => console.log(
